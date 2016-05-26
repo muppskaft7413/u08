aa985db74f0fdfbd38d9a40871ef6dd0c471a5f5
Martin <mano1321@student.miun.se>
2016-05-26 10:44:17 +0200

    Klassen medlem skapad

---
+++ Uppgift08/Uppgift08/medlem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift08
+{
+    class medlem
+    {
+        // skapat av martin och Joel, kingarna i bingarna
+    }
+}
