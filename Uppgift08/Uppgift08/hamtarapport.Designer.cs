c32e7a28eca293fe12fd9ea9dc5c054c395433cb
Martin <mano1321@student.miun.se>
2016-05-27 15:07:50 +0200

    - Skapat designen för sökfunktion enbart datum eller intervall - slutdatum kan avaktiveras eller aktiveras

--- Uppgift08/Uppgift08/hamtarapport.Designer.cs
+++ Uppgift08/Uppgift08/hamtarapport.Designer.cs
@@ -28,37 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView_rapport = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
+            this.dgvRapport = new System.Windows.Forms.DataGridView();
+            this.lblForening = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_rapport)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.gbSokDatum = new System.Windows.Forms.GroupBox();
+            this.dtpStartDatum = new System.Windows.Forms.DateTimePicker();
+            this.dtpSlutDatum = new System.Windows.Forms.DateTimePicker();
+            this.btn_klar = new System.Windows.Forms.Button();
+            this.btnSok = new System.Windows.Forms.Button();
+            this.gbSokSlutdatum = new System.Windows.Forms.GroupBox();
+            this.cbAktivSlutDatum = new System.Windows.Forms.CheckBox();
+            this.tbFeedback = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRapport)).BeginInit();
+            this.gbSokDatum.SuspendLayout();
+            this.gbSokSlutdatum.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView_rapport
+            // dgvRapport
             // 
-            this.dataGridView_rapport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_rapport.Location = new System.Drawing.Point(38, 267);
-            this.dataGridView_rapport.Name = "dataGridView_rapport";
-            this.dataGridView_rapport.Size = new System.Drawing.Size(859, 315);
-            this.dataGridView_rapport.TabIndex = 0;
+            this.dgvRapport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRapport.Location = new System.Drawing.Point(38, 267);
+            this.dgvRapport.Name = "dgvRapport";
+            this.dgvRapport.Size = new System.Drawing.Size(859, 315);
+            this.dgvRapport.TabIndex = 0;
             // 
-            // label1
+            // lblForening
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Förening:";
+            this.lblForening.AutoSize = true;
+            this.lblForening.Location = new System.Drawing.Point(35, 20);
+            this.lblForening.Name = "lblForening";
+            this.lblForening.Size = new System.Drawing.Size(51, 13);
+            this.lblForening.TabIndex = 1;
+            this.lblForening.Text = "Förening:";
             // 
             // label2
             // 
@@ -72,7 +75,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(35, 59);
+            this.label3.Location = new System.Drawing.Point(163, 20);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(34, 13);
             this.label3.TabIndex = 3;
@@ -81,83 +84,110 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(35, 72);
+            this.label4.Location = new System.Drawing.Point(203, 20);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(113, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Akrobatik/cirkushallen";
             // 
-            // groupBox1
+            // gbSokDatum
             // 
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.dateTimePicker2);
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
-            this.groupBox1.Location = new System.Drawing.Point(182, 76);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(289, 117);
-            this.groupBox1.TabIndex = 8;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Sök datum";
+            this.gbSokDatum.Controls.Add(this.dtpStartDatum);
+            this.gbSokDatum.Location = new System.Drawing.Point(38, 80);
+            this.gbSokDatum.Name = "gbSokDatum";
+            this.gbSokDatum.Size = new System.Drawing.Size(289, 65);
+            this.gbSokDatum.TabIndex = 8;
+            this.gbSokDatum.TabStop = false;
+            this.gbSokDatum.Text = "Sök datum";
             // 
-            // dateTimePicker1
+            // dtpStartDatum
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(73, 30);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 10;
+            this.dtpStartDatum.Location = new System.Drawing.Point(73, 30);
+            this.dtpStartDatum.Name = "dtpStartDatum";
+            this.dtpStartDatum.Size = new System.Drawing.Size(200, 20);
+            this.dtpStartDatum.TabIndex = 10;
             // 
-            // dateTimePicker2
+            // dtpSlutDatum
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(73, 68);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker2.TabIndex = 11;
+            this.dtpSlutDatum.Enabled = false;
+            this.dtpSlutDatum.Location = new System.Drawing.Point(83, 31);
+            this.dtpSlutDatum.Name = "dtpSlutDatum";
+            this.dtpSlutDatum.Size = new System.Drawing.Size(200, 20);
+            this.dtpSlutDatum.TabIndex = 11;
+            this.dtpSlutDatum.ValueChanged += new System.EventHandler(this.dtpSlutDatum_ValueChanged);
             // 
-            // label5
+            // btn_klar
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 30);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Förening:";
+            this.btn_klar.Location = new System.Drawing.Point(822, 590);
+            this.btn_klar.Name = "btn_klar";
+            this.btn_klar.Size = new System.Drawing.Size(75, 23);
+            this.btn_klar.TabIndex = 9;
+            this.btn_klar.Text = "Klar";
+            this.btn_klar.UseVisualStyleBackColor = true;
+            this.btn_klar.Click += new System.EventHandler(this.btn_klar_Click);
             // 
-            // label6
+            // btnSok
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 68);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Förening:";
+            this.btnSok.Location = new System.Drawing.Point(822, 238);
+            this.btnSok.Name = "btnSok";
+            this.btnSok.Size = new System.Drawing.Size(75, 23);
+            this.btnSok.TabIndex = 10;
+            this.btnSok.Text = "Sök";
+            this.btnSok.UseVisualStyleBackColor = true;
+            this.btnSok.Click += new System.EventHandler(this.btnSok_Click);
             // 
-            // label7
+            // gbSokSlutdatum
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(16, 81);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(51, 13);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Förening:";
+            this.gbSokSlutdatum.Controls.Add(this.cbAktivSlutDatum);
+            this.gbSokSlutdatum.Controls.Add(this.dtpSlutDatum);
+            this.gbSokSlutdatum.Location = new System.Drawing.Point(38, 151);
+            this.gbSokSlutdatum.Name = "gbSokSlutdatum";
+            this.gbSokSlutdatum.Size = new System.Drawing.Size(289, 64);
+            this.gbSokSlutdatum.TabIndex = 14;
+            this.gbSokSlutdatum.TabStop = false;
+            this.gbSokSlutdatum.Text = "Sök slutdatum (valfritt)";
+            // 
+            // cbAktivSlutDatum
+            // 
+            this.cbAktivSlutDatum.AutoSize = true;
+            this.cbAktivSlutDatum.Location = new System.Drawing.Point(12, 31);
+            this.cbAktivSlutDatum.Name = "cbAktivSlutDatum";
+            this.cbAktivSlutDatum.Size = new System.Drawing.Size(65, 17);
+            this.cbAktivSlutDatum.TabIndex = 15;
+            this.cbAktivSlutDatum.Text = "Aktivera";
+            this.cbAktivSlutDatum.UseVisualStyleBackColor = true;
+            this.cbAktivSlutDatum.CheckedChanged += new System.EventHandler(this.cbAktivSlutDatum_CheckedChanged);
+            // 
+            // tbFeedback
+            // 
+            this.tbFeedback.BackColor = System.Drawing.SystemColors.Info;
+            this.tbFeedback.Enabled = false;
+            this.tbFeedback.Location = new System.Drawing.Point(38, 592);
+            this.tbFeedback.Name = "tbFeedback";
+            this.tbFeedback.Size = new System.Drawing.Size(454, 20);
+            this.tbFeedback.TabIndex = 15;
             // 
             // hamtarapport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 615);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(922, 625);
+            this.Controls.Add(this.tbFeedback);
+            this.Controls.Add(this.gbSokSlutdatum);
+            this.Controls.Add(this.btnSok);
+            this.Controls.Add(this.btn_klar);
+            this.Controls.Add(this.gbSokDatum);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView_rapport);
+            this.Controls.Add(this.lblForening);
+            this.Controls.Add(this.dgvRapport);
             this.Name = "hamtarapport";
             this.Text = "hamtarapport";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_rapport)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRapport)).EndInit();
+            this.gbSokDatum.ResumeLayout(false);
+            this.gbSokSlutdatum.ResumeLayout(false);
+            this.gbSokSlutdatum.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,16 +195,18 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView_rapport;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvRapport;
+        private System.Windows.Forms.Label lblForening;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.GroupBox gbSokDatum;
+        private System.Windows.Forms.DateTimePicker dtpStartDatum;
+        private System.Windows.Forms.DateTimePicker dtpSlutDatum;
+        private System.Windows.Forms.Button btn_klar;
+        private System.Windows.Forms.Button btnSok;
+        private System.Windows.Forms.GroupBox gbSokSlutdatum;
+        private System.Windows.Forms.CheckBox cbAktivSlutDatum;
+        private System.Windows.Forms.TextBox tbFeedback;
     }
 }
