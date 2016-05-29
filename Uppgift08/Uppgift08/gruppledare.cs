ae4ceafefea2a0325260dc9273568c51a410b21d
joelelvingson <joel1501@student.miun.se>
2016-05-29 22:52:53 +0200

    Skickar ny push

---
+++ Uppgift08/Uppgift08/gruppledare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift08
+{
+    class gruppledare
+    {
+        public string förnamn { get; set; }
+        public string efternamn { get; set; }
+
+        public string gruppledaren
+        {
+            get
+            {
+                return "ledare " + förnamn + " " + efternamn;
+            }
+        }
+    }
+}
