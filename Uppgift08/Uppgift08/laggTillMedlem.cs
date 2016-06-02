06507a80d197c24413846cf97bf4d8876bbd967b
joelelvingson <joel1501@student.miun.se>
2016-06-03 00:06:44 +0200

    Ändringar gjorda.. Deletat klassen gruppmedlemmar

--- Uppgift08/Uppgift08/laggTillMedlem.cs
+++ Uppgift08/Uppgift08/laggTillMedlem.cs
@@ -25,93 +25,93 @@ namespace Uppgift08
 
         private void hamtaEjGruppmedlemmar()
         {
-            List<string> gruppLista = new List<string>();
-            foreach (traningsgrupp selectedItem in lbxTraningsgrupper.SelectedItems)
-            {
-                gruppLista.Add(selectedItem.namn);
-            }
+            //List<string> gruppLista = new List<string>();
+            //foreach (traningsgrupp selectedItem in lbxTraningsgrupper.SelectedItems)
+            //{
+            //    gruppLista.Add(selectedItem.namn);
+            //}
             
-            DataTable svarNarvaro;
+            //DataTable svarNarvaro;
 
-            postgres sokning = new postgres();
+            //postgres sokning = new postgres();
 
-            sokning.grupp = gruppLista;
-
-
-            svarNarvaro = sokning.sqlFråga(sokning.vilkenSokning(false, false, false), "hamtaEjGruppmedlemmar");     // hämtar sökning efter träningsgrupper
-
-            if (svarNarvaro.Columns[0].ColumnName.Equals("error"))
-            {
-                tbSvar.Text = svarNarvaro.Rows[0][1].ToString();
-            }
-            else
-            {
-                // här får man lägga in kod för att reda ut vilken typ av objekt o lista man vill lägga resultatet i och var datan sedan spottas ut
-                List<gruppmedlemmar> nyNarvarolista = new List<gruppmedlemmar>();
-                for (int i = 0; i < svarNarvaro.Rows.Count; i++)
-                {
-                    gruppmedlemmar narvarolistaRatt = new gruppmedlemmar()
-                    {
-                        Förnamn = svarNarvaro.Rows[i]["fnamn"].ToString(),
-                        Efternamn = svarNarvaro.Rows[i]["enamn"].ToString(),
-                        Personnummer = svarNarvaro.Rows[i]["pnr"].ToString(),
-                        medlemId = svarNarvaro.Rows[i]["medlem_id"].ToString(),
-                    };
+            //sokning.grupp = gruppLista;
 
 
-                    nyNarvarolista.Add(narvarolistaRatt);
-                    tbSvar.Text = sokOk;
-                }
+            //svarNarvaro = sokning.sqlFråga(sokning.vilkenSokning(false, false, false), "hamtaEjGruppmedlemmar");     // hämtar sökning efter träningsgrupper
 
-                lbxMedlemmar.DataSource = nyNarvarolista;
-                lbxMedlemmar.DisplayMember = "redanGruppMedlemmar";
-            }
+            //if (svarNarvaro.Columns[0].ColumnName.Equals("error"))
+            //{
+            //    tbSvar.Text = svarNarvaro.Rows[0][1].ToString();
+            //}
+            //else
+            //{
+            //    // här får man lägga in kod för att reda ut vilken typ av objekt o lista man vill lägga resultatet i och var datan sedan spottas ut
+            //    List<gruppmedlemmar> nyNarvarolista = new List<gruppmedlemmar>();
+            //    for (int i = 0; i < svarNarvaro.Rows.Count; i++)
+            //    {
+            //        gruppmedlemmar narvarolistaRatt = new gruppmedlemmar()
+            //        {
+            //            Förnamn = svarNarvaro.Rows[i]["fnamn"].ToString(),
+            //            Efternamn = svarNarvaro.Rows[i]["enamn"].ToString(),
+            //            Personnummer = svarNarvaro.Rows[i]["pnr"].ToString(),
+            //            medlemId = svarNarvaro.Rows[i]["medlem_id"].ToString(),
+            //        };
+
+
+            //        nyNarvarolista.Add(narvarolistaRatt);
+            //        tbSvar.Text = sokOk;
+            //    }
+
+            //    lbxMedlemmar.DataSource = nyNarvarolista;
+            //    lbxMedlemmar.DisplayMember = "redanGruppMedlemmar";
+            //}
         }
 
         private void hamtaGruppmedlemmar()
         {
-            List<string> gruppLista = new List<string>();
-            foreach (traningsgrupp selectedItem in lbxTraningsgrupper.SelectedItems)
-            {              
-                gruppLista.Add(selectedItem.namn);
-            }
+            //List<string> gruppLista = new List<string>();
+            //foreach (traningsgrupp selectedItem in lbxTraningsgrupper.SelectedItems)
+            //{              
+            //    gruppLista.Add(selectedItem.namn);
+            //}
 
-            DataTable svarNarvaro;
+            //DataTable svarNarvaro;
 
-            postgres sokning = new postgres();
+            //postgres sokning = new postgres();
 
-            sokning.grupp = gruppLista;
-
-
-            svarNarvaro = sokning.sqlFråga(sokning.vilkenSokning(false, false, false), "hamtaGruppmedlemmar");     // hämtar sökning efter träningsgrupper
-
-            if (svarNarvaro.Columns[0].ColumnName.Equals("error"))
-            {
-                tbSvar.Text = svarNarvaro.Rows[0][1].ToString();
-            }
-            else
-            {
-                // här får man lägga in kod för att reda ut vilken typ av objekt o lista man vill lägga resultatet i och var datan sedan spottas ut
-                List<gruppmedlemmar> nyNarvarolista = new List<gruppmedlemmar>();
-                for (int i = 0; i < svarNarvaro.Rows.Count; i++)
-                {
-
-                    gruppmedlemmar narvarolistaRatt = new gruppmedlemmar()
-                    {
-                        Förnamn = svarNarvaro.Rows[i]["fnamn"].ToString(),
-                        Efternamn = svarNarvaro.Rows[i]["enamn"].ToString(),
-                        Personnummer = svarNarvaro.Rows[i]["pnr"].ToString(),
-                        medlemId = svarNarvaro.Rows[i]["medlem_id"].ToString(),
-                    };
+            //sokning.grupp = gruppLista;
 
 
-                    nyNarvarolista.Add(narvarolistaRatt);
-                    tbSvar.Text = sokOk;
-                }
+            //svarNarvaro = sokning.sqlFråga(sokning.vilkenSokning(false, false, false), "hamtaGruppmedlemmar");     // hämtar sökning efter träningsgrupper
+
+            //if (svarNarvaro.Columns[0].ColumnName.Equals("error"))
+            //{
+            //    tbSvar.Text = svarNarvaro.Rows[0][1].ToString();
+            //}
+            //else
+            //{
+            //    // här får man lägga in kod för att reda ut vilken typ av objekt o lista man vill lägga resultatet i och var datan sedan spottas ut
+            //    List<gruppmedlemmar> nyNarvarolista = new List<gruppmedlemmar>();
+            //    for (int i = 0; i < svarNarvaro.Rows.Count; i++)
+            //    {
+
+            //        gruppmedlemmar narvarolistaRatt = new gruppmedlemmar()
+            //        {
+            //            Förnamn = svarNarvaro.Rows[i]["fnamn"].ToString(),
+            //            Efternamn = svarNarvaro.Rows[i]["enamn"].ToString(),
+            //            Personnummer = svarNarvaro.Rows[i]["pnr"].ToString(),
+            //            medlemId = svarNarvaro.Rows[i]["medlem_id"].ToString(),
+            //        };
+
+
+            //        nyNarvarolista.Add(narvarolistaRatt);
+            //        tbSvar.Text = sokOk;
+            //    }
                 
-                lbxGruppmedlemmar.DataSource = nyNarvarolista;
-                lbxGruppmedlemmar.DisplayMember = "redanGruppMedlemmar";
-            }
+            //    lbxGruppmedlemmar.DataSource = nyNarvarolista;
+            //    lbxGruppmedlemmar.DisplayMember = "redanGruppMedlemmar";
+            //}
     }
 
 
